90025ec8dfe4de6b9ffe3f0a1b1c8accfc5c03e0
arunadk1522 <99071149+arunadk1522@users.noreply.github.com>
2022-02-05 14:37:03 +0530

    Add files via upload

---
+++ addition code .cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+int main()
+{
+	float a,b,c;
+	std::cout<<"the value of a is ";
+	std::cin>>a;
+	std::cout<<"the value of b is ";
+	std::cin>>b;
+	c=a+b;
+	std::cout<<"\the sum of a and b is "<<c ;
+	return 0;
+}
